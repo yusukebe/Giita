db974660a37e29e2306c384628e6ed7bf0d5c58e
Yusuke Wada <yusukebe@cpan.org>
2010-03-18 21:27:54 +0900

    show current dir and files.

--- mojamoja.pl
+++ mojamoja.pl
@@ -1,10 +1,37 @@
+#!/usr/bin/env perl
 use Plack::Runner;
 use MojaMoja;
+use Path::Class qw/file dir/;
 
 my $app = do {
     get '/' => sub {
-        render('index.mt');
+        my $current = dir('./');
+        my @children = get_dir( $current );
+        make_response( render('dir.mt') );
     };
+
+    get "/{current}" => sub {
+        my ($req, $args) = @_;
+        my $current = $args->{current};
+        if( -d $current ) {
+            $current = dir( $current );
+            my @children = get_dir( $current );
+            make_response( render('dir.mt') );
+        }else{
+            $current = file( $current );
+            my $file = get_file( $current );
+            make_response( render('file.mt') );
+        }
+    };
+
+    sub make_response {
+        my $body = shift;
+        my $res = res(200);
+        $res->body( $body );
+        $res->content_type('text/html');
+        $res->finalize;
+    }
+
     zigorou;
 };
 
@@ -12,7 +39,29 @@ my $runner = Plack::Runner->new;
 $runner->parse_options(@ARGV);
 $runner->run($app);
 
+sub get_dir {
+    my $path = shift;
+    $path ||= dir('./');
+    return $path->children;
+}
+
+sub get_file {
+    my $path = shift;
+    return $path->slurp;
+}
+
 __DATA__
 
-@@ index.mt
-hello
+@@ dir.mt
+<h1><?= $current ?></h1>
+<ul>
+? for my $obj ( @children ) {
+<li><a href="<?= $obj ?>"><?= $obj ?></a></li>
+? }
+</ul>
+
+@@ file.mt
+<h1><?= $current ?></h1>
+<pre>
+<?= $file ?>
+</pre>

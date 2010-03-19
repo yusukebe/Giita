6feffc381e851e5aa5475c16b495435d6ec38a35
Yusuke Wada <yusukebe@cpan.org>
2010-03-19 19:18:02 +0900

    add test.

--- lib/Giita.pm
+++ lib/Giita.pm
@@ -3,7 +3,60 @@ use strict;
 use warnings;
 
 our $VERSION = '0.01';
+our $rock = 1;
 
+sub new {
+    my ( $class, %opt ) = @_;
+    my $self = bless {}, $class;
+    $self;
+}
+
+sub app {
+    my $self = shift;
+    sub {
+        my $env     = shift;
+        my $req     = Plack::Request->new($env);
+        my $current = $req->path_info;
+        $current =~ s!^/!./!;
+
+        my $base = $req->base;
+
+        if ( my $sha = $req->param('commit') ) {
+            my $git_commit = git_show($sha);
+            return make_response( render('commit.mt') );
+        }
+
+        if ( -d $current ) {
+            $current = dir($current);
+            my ( $children, $git_logs ) = get_dir($current);
+            return make_response( render('dir.mt') );
+        }
+
+        if ( -B $current ) {
+            my $body = file($current)->slurp;
+            return [ 200, [ 'Content-Length' => length $body ], [$body] ];
+        }
+
+        if ( -f $current ) {
+            $current = file($current);
+            my ( $content, $git_logs ) = get_file($current);
+            return make_response( render('file.mt') );
+        }
+        return [ 404, [], ['404 Document Not Found'] ];
+    };
+}
+
+sub make_response {
+    my $body = shift;
+    my $head = render('head.mt');
+    my $foot = render('foot.mt');
+    my $res  = res(200);
+    $res->body( $head . $body . $foot );
+    $res->content_type('text/html');
+    $res->finalize;
+}
+
+$Giita::rock;
 
 =head1 NAME
 
---
+++ t/01_basic.t
@@ -0,0 +1,8 @@
+use strict;
+use Test::More;
+
+use_ok('Giita');
+use_ok('Giita::Filter');
+use_ok('Giita::Git');
+
+done_testing;

cc780b2d7f6079790f46a02c843df20647818a56
Yusuke Wada <yusukebe@cpan.org>
2010-03-19 19:11:39 +0900

    make is as Module.

---
+++ lib/Giita.pm
@@ -0,0 +1,36 @@
+package Giita;
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+
+=head1 NAME
+
+Giita - Lightweight Rock Band.
+
+=head1 DESCRIPTION
+
+Yet another git repository viewer support perl and pod styling using "many" CPAN modules.
+
+=head1 SYNOPSIS
+
+Run on your git directory to want to see.
+
+  $ giita
+
+=head1 HOW TO GET
+
+  $ git clone git://github.com/yusukebe/Giita.git
+
+=head1 AUTHOR
+
+Yusuke Wada yusuke at kamawada.com
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
